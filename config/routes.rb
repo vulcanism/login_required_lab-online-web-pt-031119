bcad0c54e8434ab864d4a6bc0a05071c4cad9cbe
Savannah C <sgcorrell@gmail.com>
2019-08-22 16:32:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get '/show' => 'secrets#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
